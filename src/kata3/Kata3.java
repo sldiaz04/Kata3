865ffd3859da33eee38aaa141bf208c016822c41
Sergio <sldiaz04@gmail.com>
2016-10-14 11:38:20 +0100

    Kata3 v2 con la Clase Histagram

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -3,7 +3,18 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        new HistogramDisplay().execute();
+        Histogram <String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        new HistogramDisplay(histogram).execute();
     }
     
 }
