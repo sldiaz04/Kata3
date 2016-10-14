61dd6f28c2412357b0e9a98bb738c26e7a20b367
Sergio <sldiaz04@gmail.com>
2016-10-14 11:12:07 +0100

    Kata3 v1

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,45 @@
+package kata3;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame{
+
+    public HistogramDisplay() {
+        super("HISTOGRAMA DE PRUEBA");
+        setContentPane(createPanel());
+        pack();
+    }
+    
+    public void execute(){
+        setVisible(true);
+    }
+    
+    private JPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        JFreeChart chart = ChartFactory.createBarChart3D("JFreeChart Histogram", "Dominios email", "Nº de emails", dataSet, 
+                PlotOrientation.VERTICAL, 
+                false, rootPaneCheckingEnabled, rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(5, "", "gmail.com");
+        dataSet.addValue(10, "", "ulpgc.es");
+        dataSet.addValue(7, "", "ull.es");
+        dataSet.addValue(2, "", "hotmail.com");
+        return dataSet;
+    }
+}
